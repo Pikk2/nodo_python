7b1c8738e2a17559eaf5dd291d75720b3d51a0e0
pikk2 <savemail_nc@naver.com>
2023-08-15 01:56:29 +0900

    pyQt project..

---
+++ Section6_PyQt/example/pyqt_basic_1.py
@@ -0,0 +1,14 @@
+﻿import sys
+from PyQt5.QtWidgets import *
+
+app = QApplication(sys.argv)
+# print(sys.argv)
+
+label = QLabel("PyQT First Test!!")
+label.show()
+
+print("Before Loop")
+app.exec_()
+# 윈도우에 종속 되어 잇음.. 루프에 반복되면서 사용자의 응답을 기다린다!
+# while과 같이 무한루프임.. 
+print("After Loop") # 호출이 안됨..
